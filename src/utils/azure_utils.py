bf33f55a609bc2425a41c0a1f678091da2d095d7
ak3ra <akeraben@gmail.com>
2021-08-25 18:39:53 -0400

    cleaned up azure blob csv generator

--- src/utils/azure_utils.py
+++ src/utils/azure_utils.py
@@ -12,8 +12,10 @@ class DirectoryClient:
     '''
     def __init__(self, connection_string, container_name):
         service_client = BlobServiceClient.from_connection_string(connection_string)
-        container = ContainerClient.from_connection_string(connection_string, container_name)
+        self.container = ContainerClient.from_connection_string(connection_string, container_name)
         self.client = service_client.get_container_client(container_name)
+        self.connection_string = os.environ['AZURE_STORAGE_CONNECTION_STRING']
+        self.container_name = sys.argv[1]
 
     def upload_file(self, local_path, blob_name):
         '''
@@ -47,7 +49,7 @@ class DirectoryClient:
         '''
         list files in the container
         '''
-        blob_list = container.list_blobs()
+        blob_list = self.container.list_blobs()
         files = []
         for blob in blob_list:
             files.append(blob.name)
@@ -59,7 +61,7 @@ class DirectoryClient:
         create a csv file from blobs in the container
         '''
         fx = self.ls_files()
-        hotspot_paths = ["https://www."+str(CONNECTION_STRING.split(";")[1]).split("=")[1]+".blob.core.windows.net/"+CONTAINER_NAME+"/"+x for x in fx]
+        hotspot_paths = ["https://www."+str(self.connection_string.split(";")[1]).split("=")[1]+".blob.core.windows.net/"+self.container_name+"/"+x for x in fx]
         hotspot_ids = [x.split(".")[0] if "json" in x else x.split("_")[0] for x in fx]
         band_type = [x.split(".")[1] if "json" in x else x.split("_")[1].split(".")[0] for x in fx]
 
@@ -67,29 +69,3 @@ class DirectoryClient:
         dataframe = pd.DataFrame(dictionary) 
         
         return dataframe.to_csv("out.csv", index=False)
-
-    
-# main
-if __name__ == '__main__':
-
-    try:
-        CONNECTION_STRING = os.environ['AZURE_STORAGE_CONNECTION_STRING']
-    except KeyError:
-        print('AZURE_STORAGE_CONNECTION_STRING must be set')
-        sys.exit(1)
-
-    try:
-        CONTAINER_NAME = sys.argv[1]
-    except IndexError:
-        print('usage: directory_interface.py CONTAINER_NAME')
-        print('error: the following arguments are required: CONTAINER_NAME')
-        print("\n Example: python src/load_data.py sentinel")
-        sys.exit(1)
-
-
-
-    client = DirectoryClient(CONNECTION_STRING, CONTAINER_NAME)
-    container = ContainerClient.from_connection_string(CONNECTION_STRING, container_name=CONTAINER_NAME)
-
-    f = client.ls_files()
-    client.create_csv()
