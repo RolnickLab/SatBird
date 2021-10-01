26c784db5f8b3510f4c7fbda02df8f54598befbf
ak3ra <akeraben@gmail.com>
2021-10-01 00:48:13 -0400

    added bits of trainer

---
+++ src/train.py
@@ -0,0 +1,38 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.nn.modules import Module
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader, Subset
+from torchvision import models
+
+from src.dataset.dataloader import EbirdVisionDataset
+
+class EbirdTask(pl.LightningModule):
+    def config_task(self, kwargs: Any) -> None:
+        if kwargs["model"] == "resnet18":
+            self.model = models.resnet18(pretrained=False, num_classes=1)
+        else:
+            raise ValueError(f"Model type '{kwargs['model']}' is not valid")
+
+    def __init__(self, **kwargs: Any) -> None:
+        """initializes a new Lightning Module to train"""
+
+        super().__init__()
+        self.save_hyperparameters()
+        self.config_task(kwargs)
+
+    def forward(self, x:Tensor) -> Any:
+        return self.model(x)
+
+    def training_step(
+        self, batch: Dict[str, Any], batch_idx: int
+    )-> Tensor:
+        x = batch["sat"]
+        y = batch["target"]
+        y_hat = self.forward(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log("train_loss", loss)
+        
+        return loss
