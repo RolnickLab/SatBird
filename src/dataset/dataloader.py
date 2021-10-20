fix normalization + skip preprocessing step (data was corrected accordingly)

src/dataset/dataloader.py
@@ -13,7 +13,7 @@ import torch
 #from rasterio.crs import CRS
 import os
 import pandas as pd
-
+import time 
 
 def get_path(df, index, band):
     
@@ -71,24 +71,26 @@ class EbirdVisionDataset(VisionDataset):
         # band_img = get_path(self.df, index, "rgb_paths")
         # band_npy = get_path(self.df, index, "r_paths")
         band_npy = [(b,get_path(self.df, index, b)) for b in self.bands if get_path(self.df, index, b).suffix == ".npy"]
-        band_img = [(b,get_path(self.df, index, b)) for b in self.bands if is_image_file(get_path(self.df, index, b).suffix)]
+        #band_img = [(b,get_path(self.df, index, b)) for b in self.bands if is_image_file(get_path(self.df, index, b).suffix)]
         
         item_ = {}
         
         if len(band_npy) > 0:
+            #start = time.time()
+
             bands = [load_file(band) for (_,band) in band_npy]
-            bands = [b if (len(b.shape)>=3) else np.expand_dims(b, axis=0) for b in bands ]
             npy_data = np.stack(bands, axis = 1).astype(np.int32)
             item_["sat"] = torch.from_numpy(npy_data)
-            
-            
+            #elapsed = (time.time() - start)
+            #print("finished loading satellite band in " + str(elapsed))
+            #print("****************************************************")
         #TODO later if we are using img data
         #for (b, data) in band_img :                                                                           
           #  tensor_image = self.transform(image)
          #   item_[b] = tensor_image
-    
-        
+
         item_ = self.transform(item_)
+       
         #add target
         item_["target"] = None
         item_["num_complete_checklists"] = None
@@ -103,10 +105,3 @@ class EbirdVisionDataset(VisionDataset):
         
         return item_
     
-
-# if __name__ == "__main__":
-#     ebird = EbirdVisionDataset("../toydata", transform=Identity())
-#     dataset = ebird
-#     #sampler = RandomGeoSampler(ebird.index, size=1000, length=10)
-#     #dataloader = DataLoader(dataset, sampler=sampler)
-

src/trainer/trainer.py
@@ -10,14 +10,15 @@ from torchvision import models
 from src.dataset.utils import load_opts
 from src.transforms.transforms import get_transforms
 from torchvision import transforms as trsfs
-
 import pandas as pd
 import torch.nn.functional as F
 from src.losses.losses import CustomCrossEntropyLoss
-
+import torchmetrics
 
 from typing import Any, Dict, Optional
 from src.dataset.dataloader import EbirdVisionDataset
+
+import time 
 
 criterion = CustomCrossEntropyLoss()#BCEWithLogitsLoss()
 m = nn.Sigmoid()
@@ -25,12 +26,11 @@ m = nn.Sigmoid()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class EbirdTask(pl.LightningModule):
-    def __init__(self, opts = '/home/mila/t/tengmeli/ecosystem-embedding/configs/defaults.yaml',**kwargs: Any) -> None:
+    def __init__(self, opts = 'configs/defaults.yaml',**kwargs: Any) -> None:
         """initializes a new Lightning Module to train"""
 
         super().__init__()
         self.save_hyperparameters()
-        
         self.config_task(opts, **kwargs)
         self.opts = load_opts(opts)
         
@@ -45,8 +45,8 @@ class EbirdTask(pl.LightningModule):
             self.loss = CustomCrossEntropyLoss() #BCEWithLogitsLoss()
             self.m = nn.Sigmoid()
             self.criterion = CustomCrossEntropyLoss()
-            
-        
+            self.mse = torchmetrics.MeanSquaredError()
+            self.mae = torchmetrics.MeanAbsoluteError()
 
         else:
             raise ValueError(f"Model type '{self.opts.experiment.module.model}' is not valid")
@@ -62,10 +62,16 @@ class EbirdTask(pl.LightningModule):
         
         x = batch['sat'].squeeze(1).to(device)
         y = batch['target'].to(device)
-    
+
         y_hat = self.forward(x)
-        loss = self.loss(m(y_hat), y)
+        pred = m(y_hat)
+        loss = self.loss(pred, y)
+        mse = self.mse(pred, y)
+        mae = self.mae(pred, y)
+
         self.log("train_loss", loss)
+        self.log("train_mae", mae)
+        self.log("train_mse", mse )
         
         return loss
 
@@ -80,7 +86,14 @@ class EbirdTask(pl.LightningModule):
         
         y_hat = self.forward(x)
         loss = self.loss(m(y_hat), y)
+        pred = m(y_hat)
+        loss = self.loss(pred, y)
+        mse = self.mse(pred, y)
+        mae = self.mae(pred, y)
         self.log("val_loss", loss)
+        self.log("val_mae", mae)
+        self.log("val_mse", mse )
+    
 
     def test_step(
         self, batch: Dict[str, Any], batch_idx:int
@@ -90,13 +103,18 @@ class EbirdTask(pl.LightningModule):
         x = batch['sat'].squeeze(1).to(device)
         y = batch['target'].to(device)
         y_hat = self.forward(x)
-        loss = self.loss(m(y_hat), y)
-        self.log("Test Loss", loss)
+        pred = m(y_hat)
+        loss = self.loss(pred, y)
+        mse = self.mse(pred, y)
+        mae = self.mae(pred, y)
+        self.log("test_loss", loss)
+        self.log("test_mae", mae)
+        self.log("test_mse", mse )
 
     def configure_optimizers(self) -> Dict[str, Any]:
         optimizer = torch.optim.Adam(
             self.model.parameters(),
-            lr = self.opts.experiment.module.lr, #CHECK IN CONFIG
+            lr=self.opts.experiment.module.lr,  #CHECK IN CONFIG
         )
         return{
             "optimizer": optimizer,
@@ -105,7 +123,7 @@ class EbirdTask(pl.LightningModule):
                     optimizer,
                     patience = self.opts.experiment.module.lr_schedule_patience   #CHECK IN CONFIG
             ),
-            "monitor":"Val lloss",
+            "monitor":"val_loss",
             }
         }
 
@@ -145,8 +163,7 @@ class EbirdDataModule(pl.LightningDataModule):
 
         )
 
-        self.all_test_dataset = EbirdVisionDataset(
-                                
+        self.all_test_dataset = EbirdVisionDataset(                
             self.df_test, 
             bands = self.bands,
             split = "test",

src/transforms/transforms.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 from torch import Tensor
 from torch.nn import Module  # type: ignore[attr-defined]
-
+import torch.nn.functional as F
 # https://github.com/pytorch/pytorch/issues/60979
 # https://github.com/pytorch/pytorch/pull/61045
 Module.__module__ = "torch.nn"
@@ -92,8 +92,12 @@ class Normalize:
                 for task, tensor in sample.items() if task in transformable
             }
         #TODO 
-        #if self.custom:
-        #    mean, std = self.custom
+        if self.custom:
+            means, std = custom
+            d = {
+                task: F.normalize(tensor, means, std)
+                 for task, tensor in sample.items() if task in transformable
+            }
         #    pass
         return(d)
 
@@ -197,7 +201,7 @@ def get_transform(transform_item, mode):
     elif transform_item.name == "normalize" and not (
         transform_item.ignore is True or transform_item.ignore == mode
     ):
-        return Normalize(maxchan=True, custom=transform_item.mean_std or None)
+        return Normalize(maxchan=transform_item.maxchan, custom=transform_item.mean_std or None)
 
     elif transform_item.ignore is True or transform_item.ignore == mode:
         return None