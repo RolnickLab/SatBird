8d338565b46939b1755a1d2afea17d5a0f328b6c
melisandeteng <melisande.teng@student.ecp.fr>
2021-10-22 18:09:37 -0400

    add params + need to debug nan loss

--- src/losses/losses.py
+++ src/losses/losses.py
@@ -5,5 +5,6 @@ import torch.nn as nn
 class CustomCrossEntropyLoss(nn.Module):
     def __init__(self):
         super().__init__()
+
     def __call__(self, pred, target):
         return (-target * torch.log(pred) - (1-target) *torch.log(1 - pred)).sum()
--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.nn.modules import Module
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import ReduceLROnPlateau, StepLR
 from torch.utils.data import DataLoader, Subset
 from torchvision import models
 
@@ -25,6 +25,16 @@ m = nn.Sigmoid()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+def get_scheduler(optimizer, opts):
+    if opts.scheduler.name == "ReduceLROnPlateau":
+        return (ReduceLROnPlateau(optimizer,
+                  patience = opts.scheduler.reduce_lr_plateau.lr_schedule_patience))
+    elif opts.scheduler.name == "StepLR":
+        return (StepLR(optimizer, opts.scheduler.step_lr.step_size, opts.scheduler.step_lr.gamma))
+    else:
+        raise ValueError(f"Scheduler'{self.opts.scheduler.name}' is not valid")
+        
+
 class EbirdTask(pl.LightningModule):
     def __init__(self, opts = 'configs/defaults.yaml',**kwargs: Any) -> None:
         """initializes a new Lightning Module to train"""
@@ -39,12 +49,26 @@ class EbirdTask(pl.LightningModule):
         self.opts = load_opts(opts)
         
         if self.opts.experiment.module.model == "resnet18":
+            
             self.model = models.resnet18(pretrained=True)
             self.model.fc = nn.Linear(512, 684) 
             self.model.to(device)
             self.loss = CustomCrossEntropyLoss() #BCEWithLogitsLoss()
             self.m = nn.Sigmoid()
+            
             self.criterion = CustomCrossEntropyLoss()
+            #self.opts.losses.ce.lambd_pres,self.opts.losses.ce.lambd_abs
+            self.mse = torchmetrics.MeanSquaredError()
+            self.mae = torchmetrics.MeanAbsoluteError()
+            
+        elif self.opts.experiment.module.model == "resnet50":
+            self.model = models.resnet50(pretrained=True)
+            self.model.fc = nn.Linear(512, 684) 
+            self.model.to(device)
+            self.loss = CustomCrossEntropyLoss() #BCEWithLogitsLoss()
+            self.m = nn.Sigmoid()
+            
+            self.criterion = CustomCrossEntropyLoss()#self.opts.losses.ce.lambd_pres,self.opts.losses.ce.lambd_abs)
             self.mse = torchmetrics.MeanSquaredError()
             self.mae = torchmetrics.MeanAbsoluteError()
 
@@ -62,15 +86,18 @@ class EbirdTask(pl.LightningModule):
         
         x = batch['sat'].squeeze(1).to(device)
         y = batch['target'].to(device)
-
+        
         y_hat = self.forward(x)
+        
         pred = m(y_hat)
+        
         loss = self.loss(pred, y)
+
         mse = self.mse(pred, y)
         mae = self.mae(pred, y)
         self.log("train_loss", loss)
-        self.log("train_mae", mae)
-        self.log("train_mse", mse )
+        self.log("train_mae", mae * self.opts.losses.mae.scale)
+        self.log("train_mse", mse * self.opts.losses.mse.scale )
         
         return loss
 
@@ -84,14 +111,14 @@ class EbirdTask(pl.LightningModule):
         y = batch['target'].to(device)
         
         y_hat = self.forward(x)
-        loss = self.loss(m(y_hat), y)
+        
         pred = m(y_hat)
         loss = self.loss(pred, y)
         mse = self.mse(pred, y)
         mae = self.mae(pred, y)
         self.log("val_loss", loss)
-        self.log("val_mae", mae)
-        self.log("val_mse", mse )
+        self.log("val_mae", mae * self.opts.losses.mae.scale)
+        self.log("val_mse", mse * self.opts.losses.mse.scale )
     
 
     def test_step(
@@ -103,28 +130,30 @@ class EbirdTask(pl.LightningModule):
         y = batch['target'].to(device)
         y_hat = self.forward(x)
         pred = m(y_hat)
+        
         loss = self.loss(pred, y)
         mse = self.mse(pred, y)
         mae = self.mae(pred, y)
         self.log("test_loss", loss)
-        self.log("test_mae", mae)
-        self.log("test_mse", mse )
+        self.log("test_mae", mae * self.opts.losses.mae.scale)
+        self.log("test_mse", mse * self.opts.losses.mse.scale)
 
     def configure_optimizers(self) -> Dict[str, Any]:
         optimizer = torch.optim.Adam(
             self.model.parameters(),
             lr=self.opts.experiment.module.lr,  #CHECK IN CONFIG
         )
+        
+        scheduler = get_scheduler(optimizer, self.opts)
+        
         return{
             "optimizer": optimizer,
             "lr_scheduler": {
-                "scheduler": ReduceLROnPlateau(
-                    optimizer,
-                    patience = self.opts.experiment.module.lr_schedule_patience   #CHECK IN CONFIG
-            ),
+                "scheduler": scheduler,
             "monitor":"val_loss",
             }
         }
+
 
 class EbirdDataModule(pl.LightningDataModule):
     def __init__(self, opts) -> None:
@@ -134,7 +163,6 @@ class EbirdDataModule(pl.LightningDataModule):
         self.seed = self.opts.program.seed
         self.batch_size = self.opts.data.loaders.batch_size
         self.num_workers = self.opts.data.loaders.num_workers
-
         self.df_train = pd.read_csv(self.opts.data.files.train)
         self.df_val = pd.read_csv(self.opts.data.files.val)
         self.df_test = pd.read_csv(self.opts.data.files.test)
