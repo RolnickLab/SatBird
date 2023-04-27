8cce8bbee288b6f2747d10ceb88e26c8c6c209df
hagerrady13 <hagerradi@aucegypt.edu>
2023-04-26 22:29:38 -0600

    changes to run test

--- test2.py
+++ test2.py
@@ -1,29 +1,23 @@
 import comet_ml
 import os
-import sys
 from pathlib import Path
 from os.path import expandvars
-#sys.path.append(str(Path().resolve().parent))
-#sys.path.append(str(Path().resolve().parent.parent))
 import hydra
+from hydra.utils import get_original_cwd
 from addict import Dict
 from omegaconf import OmegaConf, DictConfig
+from typing import Any, Dict, cast
+
 from src.trainer.trainer import EbirdTask, EbirdDataModule
-#from src.trainer.trainer_species import EbirdSpeciesTask
 import src.trainer.geo_trainer as geo_trainer
 import src.trainer.state_trainer as state_trainer
 import src.trainer.multires_trainer as multires_trainer
+from src.dataset.utils import set_data_paths
 
 import pytorch_lightning as pl
-from pytorch_lightning import loggers as pl_loggers
-from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import CometLogger
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
-from typing import Any, Dict, Tuple, Type, cast
-from src.dataset.utils import set_data_paths
-import pdb
 
 hydra_config_path = Path(__file__).resolve().parent / "configs/hydra.yaml"
+
 
 def resolve(path):
     """
@@ -35,14 +29,14 @@ def resolve(path):
     return Path(expandvars(str(path))).expanduser().resolve()
 
 
-def set_up_omegaconf()-> DictConfig:
+def set_up_omegaconf() -> DictConfig:
     """Helps with loading config files"""
-    
+
     conf = OmegaConf.load("./configs/defaults.yaml")
     command_line_conf = OmegaConf.from_cli()
 
     if "config_file" in command_line_conf:
-        
+
         config_fn = command_line_conf.config_file
 
         if os.path.isfile(config_fn):
@@ -56,10 +50,11 @@ def set_up_omegaconf()-> DictConfig:
     )
     conf = set_data_paths(conf)
     conf = cast(DictConfig, conf)  # convince mypy that everything is alright
-    
-    #if commandline_opts is not None and isinstance(commandline_opts, dict):
+
+    # if commandline_opts is not None and isinstance(commandline_opts, dict):
     #    opts = Dict(merge(commandline_opts, opts))
     return conf
+
 
 def load_opts(path, default, commandline_opts):
     """
@@ -70,12 +65,12 @@ def load_opts(path, default, commandline_opts):
             which needs processing such as setting default values for lambdas and gen
             fields
      """
-    
+
     if path is None and default is None:
         path = (
-            resolve(Path(__file__)).parent.parent
-            / "config"
-            / "defaults.yaml"
+                resolve(Path(__file__)).parent.parent
+                / "config"
+                / "defaults.yaml"
         )
         print(path)
     else:
@@ -95,12 +90,11 @@ def load_opts(path, default, commandline_opts):
     else:
         print("using config ", path)
         overriding_opts = OmegaConf.load(path)
-        
 
     opts = OmegaConf.merge(default_opts, overriding_opts)
 
     if commandline_opts is not None and isinstance(commandline_opts, dict):
-        opts =  OmegaConf.merge(opts, commandline_opts)
+        opts = OmegaConf.merge(opts, commandline_opts)
         print("Commandline opts", commandline_opts)
 
     conf = set_data_paths(opts)
@@ -108,45 +102,36 @@ def load_opts(path, default, commandline_opts):
     return conf
 
 
-    
-@hydra.main(config_path="configs", config_name = "hydra") #hydra_config_path)
+@hydra.main(config_path="configs", config_name="hydra")
 def main(opts):
-
     hydra_opts = dict(OmegaConf.to_container(opts))
     print("hydra_opts", hydra_opts)
     args = hydra_opts.pop("args", None)
 
-    config_path = "/home/mila/t/tengmeli/ecosystem-embedding/configs/base.yaml"
-    #args['config'] #"/home/mila/t/tengmeli/ecosystem-embedding/configs/custom_meli_2.yaml" 
-    default = "/home/mila/t/tengmeli/ecosystem-embedding/configs/defaults.yaml" #args['default']
-    #default = Path(__file__).parent / "configs/defaults.yaml"
-    conf = load_opts(config_path, default=default, commandline_opts=hydra_opts)
-    conf.save_path = conf.save_path+os.environ["SLURM_JOB_ID"]
+    base_dir = args['base_dir']
+    if not base_dir:
+        base_dir = get_original_cwd()
+
+    config_path = os.path.join(base_dir, args['config'])
+    default_config = os.path.join(base_dir, "configs/defaults.yaml")
+
+    conf = load_opts(config_path, default=default_config, commandline_opts=hydra_opts)
+    conf.save_path = os.path.join(base_dir, conf.save_path, os.environ["SLURM_JOB_ID"])
     pl.seed_everything(conf.program.seed)
 
-    #if not os.path.exists(conf.save_preds_path):
-     #   os.makedirs(conf.save_preds_path)
-    #with open(os.path.join(conf.save_preds_path, "config.yaml"),"w") as fp:
-     #   OmegaConf.save(config = conf, f = fp)
-    #fp.close()
-    
-    print(conf.log_comet)
-    
-    print(conf)
     print('multires len:', len(conf.data.multiscale))
-    if not conf.loc.use and len(conf.data.multiscale)>1:
-         print('using multiscale net')
-         task = multires_trainer.EbirdTask(conf)
-         datamodule = EbirdDataModule(conf)
+    if not conf.loc.use and len(conf.data.multiscale) > 1:
+        print('using multiscale net')
+        task = multires_trainer.EbirdTask(conf)
+        datamodule = EbirdDataModule(conf)
     elif "speciesAtoB" in conf.keys() and conf.speciesAtoB:
-            print("species A to B")
-            task = EbirdSpeciesTask(conf)
-            datamodule = EbirdDataModule(conf)
-    elif not conf.loc.use :
+        print("species A to B")
+        task = EbirdSpeciesTask(conf)
+        datamodule = EbirdDataModule(conf)
+    elif not conf.loc.use:
         task = EbirdTask(conf)
         datamodule = EbirdDataModule(conf)
-   
-        
+
     elif conf.loc.loc_type == "latlon":
         print("Using geo information")
         task = geo_trainer.EbirdTask(conf)
@@ -155,30 +140,26 @@ def main(opts):
         print("Using geo information")
         task = state_trainer.EbirdTask(conf)
         datamodule = state_trainer.EbirdDataModule(conf)
-        
-    
+
     trainer_args = cast(Dict[str, Any], OmegaConf.to_object(conf.trainer))
-    if conf.load_ckpt_path != "":
+    if conf.load_ckpt_path:
         print("Loading existing checkpoint")
-    conf.load_ckpt_path = "/network/scratch/t/tengmeli/ecosystem-embeddings/checkpoint_base_resnet18_allspecies2760900/last.ckpt" #last.ckpt"
-    #"/network/scratch/a/amna.elmustafa/ecosystem-embeddings/ckpts2527294/last.ckpt"
-    #above with landuse : /network/scratch/a/amna.elmustafa/ecosystem-embeddings/ckpts2527309
-    #sat landuse env 512  /network/scratch/a/amna.elmustafa/ecosystem-embeddings/ckpts2527306
-    #Sat landuse  env 224 /network/scratch/a/amna.elmustafa/ecosystem-embeddings/ckpts2527294
-    
-    task = task.load_from_checkpoint(conf.load_ckpt_path, save_preds_path = conf.save_preds_path)    
-        
-    trainer = pl.Trainer(**trainer_args)
-    trainer.validate(model=task, datamodule=datamodule)
-    trainer.test(model=task, 
-                       dataloaders=datamodule.test_dataloader(),
-               
-                       #ckpt_path='best',
-                       verbose=True)
-    #trainer.test(model=task, dataloaders=datamodule.train_dataloader(),verbose=True)
-    
-    
+        # "/network/scratch/a/amna.elmustafa/ecosystem-embeddings/ckpts2527294/last.ckpt"
+        # above with landuse : /network/scratch/a/amna.elmustafa/ecosystem-embeddings/ckpts2527309
+        # sat landuse env 512  /network/scratch/a/amna.elmustafa/ecosystem-embeddings/ckpts2527306
+        # Sat landuse  env 224 /network/scratch/a/amna.elmustafa/ecosystem-embeddings/ckpts2527294
+        print("Checkpoint: ", os.path.join(base_dir, conf.load_ckpt_path))
+        task = task.load_from_checkpoint(os.path.join(base_dir, conf.load_ckpt_path),
+                                         save_preds_path=conf.save_preds_path)
+
+        trainer = pl.Trainer(**trainer_args)
+        trainer.validate(model=task, datamodule=datamodule)
+        trainer.test(model=task,
+                     dataloaders=datamodule.test_dataloader(),
+                     verbose=True)
+    else:
+        print("No checkpoint provided...")
+
+
 if __name__ == "__main__":
     main()
-
-    
