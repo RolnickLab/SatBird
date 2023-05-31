056e522de1b8bedc0cd0e353a21063000fd7ace4
Hager Rady <10363680+hagerrady13@users.noreply.github.com>
2023-05-31 17:05:32 -0600

    Update sbatch_RM_meli.sh

--- data_processing/ebird_data_preparation/sbatch_RM_meli.sh
+++ data_processing/ebird_data_preparation/sbatch_RM_meli.sh
@@ -3,10 +3,10 @@
 #SBATCH --output=job_output_RM.txt
 #SBATCH --error=job_error_RM.txt
 #SBATCH --ntasks=1
-#SBATCH --time=23:59:00
-#SBATCH --mem-per-cpu=50Gb
+#SBATCH --time=04:59:00
+#SBATCH --mem-per-cpu=10Gb
 #SBATCH --cpus-per-task=8
-#SBATCH --partition=long
+#SBATCH --partition=long-cpu
 
 ### this specifies the length of array
 #SBATCH --array=9-16:1
