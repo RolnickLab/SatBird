37c7c82c20862aafb1656dac676f69a0c655687c
Hager Rady <10363680+hagerrady13@users.noreply.github.com>
2023-05-31 21:45:31 -0600

    Update sbatch_RM_meli.sh

--- data_processing/ebird_data_preparation/sbatch_RM_meli.sh
+++ data_processing/ebird_data_preparation/sbatch_RM_meli.sh
@@ -3,13 +3,13 @@
 #SBATCH --output=job_output_RM.txt
 #SBATCH --error=job_error_RM.txt
 #SBATCH --ntasks=1
-#SBATCH --time=04:59:00
+#SBATCH --time=10:59:00
 #SBATCH --mem-per-cpu=10Gb
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=long-cpu
 
 ### this specifies the length of array
-#SBATCH --array=9-16:1
+#SBATCH --array=9-12:1
 
 #SBATCH --mail-user=$USER@mila.quebec
 #SBATCH --mail-type=FAIL
