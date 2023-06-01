83823f37c0079e1ea2498517ad8b9057cc5441f5
melisandeteng <melisande.teng@student.ecp.fr>
2023-06-01 18:51:13 -0400

    fix reading files problem

--- data_processing/utils/save_range_maps_csv.py
+++ data_processing/utils/save_range_maps_csv.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 import pandas as pd
-
+from tqdm import tqdm
 
 def save_range_maps_csv(save_path):
     range_path = "/network/projects/ecosystem-embeddings/ebird_dataset/USA_summer/range_maps/"
@@ -12,12 +12,15 @@ def save_range_maps_csv(save_path):
         species = [line.strip("\n") for line in f.readlines()]
 
     range_map_csv = pd.DataFrame(columns = species)
-
-    for file in files:
+    range_map_csv["hotspot_id"]=""
+    for file in tqdm(files):
         data = pickle.load(open(os.path.join(range_path, file), "rb"))
-        key= list(data.keys())[-1]
-        range_map_csv = range_map_csv.append(data[key], ignore_index=True)
+        if "loc" in list(data.keys()):
+            key= list(data.keys())[-1]
+            data = data[key]
+        data["hotspot_id"] = file
+        range_map_csv = range_map_csv.append(data, ignore_index=True)
     range_map_csv.to_csv(save_path)
 
 if __name__=="__main__":
-    save_range_maps_csv("/network/projects/ecosystem-embeddings/ebird_dataset/USA_summer/range_maps_species.csv")
+    save_range_maps_csv("/network/projects/ecosystem-embeddings/ebird_dataset/USA_summer/range_maps_species_part_1.csv")
