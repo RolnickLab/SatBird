83acd1db314b80859eaf0b2bd5faf808ae8d2f42
benjamin akera <akeraben@gmail.com>
2023-06-07 10:39:20 -0400

    added tests.sh

---
+++ run_test.sh
@@ -0,0 +1,3 @@
+python test.py args.config=configs/base_kenya.yaml
+python test.py args.config=configs/kenya_transfer_learning.yaml
+python test.py args.config=configs/kenya_transferlearning_frozen.yaml
