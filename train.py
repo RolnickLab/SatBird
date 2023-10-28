2b9e1b261f92f94bfde87a5bab15115ced9e4abd
hagerrady13 <hagerradi@aucegypt.edu>
2023-10-28 01:23:23 -0600

    changes to normalization configs for generalization

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def main(opts):
     config.base_dir = base_dir
 
     # compute means and stds for normalization
-    if config.data.env and not config.data.transforms.normalize.ignore:
+    if len(config.data.env) > 0:
         config.variables.bioclim_means, config.variables.bioclim_std, config.variables.ped_means, config.variables.ped_std = compute_means_stds_env_vars(
             root_dir=config.data.files.base,
             train_csv=config.data.files.train,
@@ -50,13 +50,13 @@ def main(opts):
             output_file_std=config.data.files.env_stds
             )
 
-    if config.data.datatype == "refl" and not config.data.transforms[4].ignore :
+    if config.data.datatype == "refl":
         config.variables.rgbnir_means, config.variables.rgbnir_std = compute_means_stds_images(
             root_dir=config.data.files.base,
             train_csv=config.data.files.train,
             output_file_means=config.data.files.rgbnir_means,
             output_file_std=config.data.files.rgbnir_stds)
-    elif config.data.datatype == "img" and not config.data.transforms[4].ignore:
+    elif config.data.datatype == "img":
         config.variables.visual_means, config.variables.visual_stds = compute_means_stds_images_visual(
             root_dir=config.data.files.base,
             train_csv=config.data.files.train,
