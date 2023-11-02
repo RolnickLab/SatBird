d5c4c7d1c5d8ac1d67c3869db4136a4bf645590f
hagerrady13 <hagerradi@aucegypt.edu>
2023-11-02 13:25:03 -0600

    running baselines

--- src/models/vit.py
+++ src/models/vit.py
@@ -67,7 +67,7 @@ def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
     out: (M, D)
     """
     assert embed_dim % 2 == 0
-    omega = np.arange(embed_dim // 2, dtype=np.float)
+    omega = np.arange(embed_dim // 2, dtype=float)
     omega /= embed_dim / 2.
     omega = 1. / 10000 ** omega  # (D/2,)
 
@@ -214,7 +214,6 @@ class ViTFinetune(VisionTransformer):
         del self.decoder_pred
 
     def forward(self, x):
-        print('in vit finetune')
         B = x.shape[0]
         x = self.patch_embed(x)
 
