970b5024f2ee6d659f29dc7fcaea354d5f2fb58d
melisandeteng <melisande.teng@student.ecp.fr>
2023-11-06 10:08:57 -0500

    modify output file name to avoid error

--- run_files/multiple_runs.sh
+++ run_files/multiple_runs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=ebird_baseline
-#SBATCH --output=job_output.txt
-#SBATCH --error=job_error.txt
+#SBATCH --output=job_output_%j.txt
+#SBATCH --error=job_error_%j.txt
 #SBATCH --ntasks=1
 #SBATCH --time=34:59:00
 #SBATCH --mem-per-cpu=50Gb
@@ -14,11 +14,10 @@
 
 # load conda environment
 module load anaconda/3
-conda activate eco
+conda activate satbird
 
 # export keys for logging, etc,
 export COMET_API_KEY=$COMET_API_KEY
 export HYDRA_FULL_ERROR=1
-
 # run training script
-python train.py args.config=configs/SatBird-USA-summer/resnet18_RGBNIR.yaml args.run_id=$SLURM_ARRAY_TASK_ID
+python train.py args.config=configs/SatBird-USA-summer/resnet18_RGBNIR_ENV_RM.yaml args.run_id=$SLURM_ARRAY_TASK_ID
